f3982b24f68ddf685d345e9a537e6ea2e34ee665
pablo <pagoru@gmail.com>
2020-03-22 18:14:01 +0100

    Auto-Update lib

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,18 @@
 
-console.log('Hello world!');
+const handlerDispatch = [];
+
+export const middlewareHandler = (
+    willDispatch: (action: { type: string, data: any }, getState) => Object,
+    didDispatch: (action: { type: string, data: any }, getState) => void
+) => {
+    handlerDispatch.push({ willDispatch, didDispatch });
+};
+
+export const middleware = ({ getState }) => {
+    return next => action => {
+        handlerDispatch.forEach(handler => action = handler.willDispatch(action, getState));
+        const returnValue = next(action);
+        handlerDispatch.forEach(handler => handler.didDispatch(action, getState));
+        return returnValue;
+    }
+};
